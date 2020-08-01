ffb7a8e8c98b132bab0d6813fb2734223ef2f391
Nirmal Narayanan <nirmal.narayanan04@gmail.com>
2020-08-01 21:01:30 +0530

    Logging to show Service worker registration state

--- public/javascripts/utils/SwHelper.js
+++ public/javascripts/utils/SwHelper.js
@@ -10,6 +10,9 @@ class SwHelper {
         .register('pixelSw.js', { scope: '/' })
         .then((registration) => {
           console.log('Service worker registered for', registration.scope);
+          registration.addEventListener('updatefound', () => {
+            this.handleSwUpdateFound(registration)
+          }) 
         }).catch((error) => {
           console.log('Service worker registration failed ', error);
         });
@@ -17,6 +20,24 @@ class SwHelper {
       console.log('Service workers are not supported');
     }
   }
+
+  handleSwUpdateFound = (registration) => {
+    const installingWorker = registration.installing;
+
+    if (installingWorker) {
+      console.log('Installing new service worker: ', installingWorker);
+      installingWorker.onstatechange = () => {
+        if (installingWorker.state === 'installed') {
+          if (navigator.serviceWorker.controller) {
+            console.log(`Service worker updated, will be used when all
+            other tabs are closed.`);
+          } else {
+            console.log('Service worker installed');
+          }
+        }
+      };
+    }
+  };
 
 }
 
