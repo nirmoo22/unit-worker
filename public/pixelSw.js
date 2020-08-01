2197d95f6b97690ac6546402508d6565ac50bf4c
Nirmal Narayanan <nirmal.narayanan04@gmail.com>
2020-08-01 19:08:13 +0530

    Intercept and modify request for pixel.gif in serviceworker

--- public/pixelSw.js
+++ public/pixelSw.js
@@ -1,4 +1,14 @@
-self.addEventListener('install', (event) => {
+const urlParamsMapping = {
+  interaction: 'event',
+  client: 'customer',
+  os_name: 'operating_system_name',
+  x1: 'utm_source',
+  x2: 'utm_medium',
+  x3: 'utm_campaign',
+  landing_url: 'campaign_url'
+}
+
+const handleInstallEvent = (event) => {
   console.log('Installing Service Worker...')
   event.waitUntil(
     caches.open('cache-v1').then((cache) => {
@@ -12,4 +22,43 @@ self.addEventListener('install', (event) => {
       console.log('Failed to set cache on install ', error)
     })
   );
-});
+}
+
+const handleActivateEvent = (event) => {
+  event.waitUntil(clients.claim());
+}
+
+const fetchInterceptor = (event) => {
+  if (event.request.url.indexOf('pixel.gif') !== -1) {
+    handlePixelRequest(event)
+  }
+}
+
+
+/**
+ * Handles all request for /pixel.gif
+ * @param {Event} event 
+ */
+const handlePixelRequest = (event) => {
+  event.respondWith(
+    new Response('Response from Service Worker :)', {
+      headers: { 'Content-Type': 'text/plain' }
+    })
+  );
+
+  let requestUrl = new URL(event.request.url);
+  const searchParamsFromPage = new URLSearchParams(requestUrl.search)
+  let newSearchParams = {};
+  for (const key of Object.keys(urlParamsMapping)) {
+    if (urlParamsMapping.hasOwnProperty(key))
+      newSearchParams[urlParamsMapping[key]] = searchParamsFromPage.get(key)
+  }
+  requestUrl.search = new URLSearchParams(newSearchParams).toString();
+  fetch(requestUrl)
+}
+
+
+// add events:
+self.addEventListener('install', handleInstallEvent);
+self.addEventListener('activate', handleActivateEvent);
+self.addEventListener('fetch', fetchInterceptor);
